743ad60346370527fc056eb5d03afee3c5cbfb29
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-04-10 22:09:59 -0500

    jueguito terminado

---
+++ santiagoburga/Assets/Hazard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Enter en tirgger con" + other);
+		if (other.CompareTag ("Player")){
+			other.GetComponent<Movimiento> ().ResetPosition ();
+		}
+	}
+}
---
+++ santiagoburga/Assets/Movimiento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public Vector3 initialposition; 
+	public float speed;
+	public Vector3 limit;
+	// Use this for initialization
+	void Start () {
+        initialposition = transform.position;
+ 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.W) && transform.position.z < limit.z) {
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S) && transform.position.z > -limit.z) {
+			transform.Translate (Vector3.back * speed * Time.deltaTime);
+		}
+    }
+     public void ResetPosition(){
+        transform.position = initialposition;
+    
+    }
+}
